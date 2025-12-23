6059623eacefef9b8b7d6a1ff1018967cb7e2fb4
Mahmoud Younis <mahmoud.abedallah654@science.aun.edu.eg>
2025-12-23 22:04:09 +0200

    feat: implement app sections with bottom navigation in the home screen

---
+++ lib/home_screen.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+import 'package:online_exam_app/core/theme/app_colors.dart';
+import 'package:online_exam_app/features/cart/cart_screen.dart';
+import 'package:online_exam_app/features/categorey/categorey_screen.dart';
+import 'package:online_exam_app/features/home/presentation/views/home_tap.dart';
+import 'package:online_exam_app/features/profile/profile_screen.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  int _currentIndex = 0;
+  final List<Widget> _taps = [
+    const HomeTap(),
+    const CategoriesTap(),
+    const CartTap(),
+    const ProfileTap(),
+  ];
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      bottomNavigationBar: BottomNavigationBar(
+        elevation: 0,
+        backgroundColor: AppColors.background,
+        onTap: (index) {
+          if (_currentIndex == index) return;
+          _currentIndex = index;
+          setState(() {});
+        },
+        type: BottomNavigationBarType.fixed,
+        currentIndex: _currentIndex,
+        selectedItemColor: AppColors.primary,
+        unselectedItemColor: AppColors.iconGrey,
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home_outlined),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.category_outlined),
+            label: 'Categories',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.shopping_cart_outlined),
+            label: 'Cart',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person_outline),
+            label: 'Profile',
+          ),
+        ],
+      ),
+      body: _taps[_currentIndex],
+    );
+  }
+}
