1c7aa51fe39fae600ea7237f978a0ce07d5f7e9f
‪Ahmed Salah Ahmed‬‏ <AhmedSalah.92.2025@gmail.com>
2025-12-24 18:55:50 +0200

    feat: Add data layer for forget password feature
    - **Data Source:**
        - `ForgetPasswordRemoteDataSource`: Added an abstract interface for handling remote forget password, OTP verification, and password reset operations.
    
    - **Repository Implementation:**
        - `ForgetPasswordRepoImpl`: Implemented the `ForgetPasswordRepo` interface, connecting the remote data source to the domain layer.
        - Utilized `safeApiCall` to handle API requests and responses for forget password, verify OTP, and reset password functionalities, ensuring error handling and data consistency.
    
    - **Models:**
        - `ForgetPasswordResponse`: Represents the data structure for the forget password API response and includes a method to map it to a `ForgetPasswordEntity`.
        - `VerifyOtpCodeResponse`: Defines the response model for OTP code verification and its mapping to a `VerifyOtpCodeEntity`.
        - `ResetPasswordResponse`: Models the response for the password reset action, including a method to convert it to a `ResetPasswordEntity`.

---
+++ lib/features/auth/forget_password/data/models/reset_password_response/reset_password_response.dart
@@ -0,0 +1,19 @@
+import '../../../domain/entities/reset_password_entity.dart';
+import 'package:json_annotation/json_annotation.dart';
+
+part 'reset_password_response.g.dart';
+
+@JsonSerializable()
+class ResetPasswordResponse {
+  ResetPasswordResponse({this.message, this.token});
+
+  factory ResetPasswordResponse.fromJson(Map<String, dynamic> json) {
+    return _$ResetPasswordResponseFromJson(json);
+  }
+  String? message;
+  String? token;
+
+  Map<String, dynamic> toJson() => _$ResetPasswordResponseToJson(this);
+  ResetPasswordEntity toEntity() =>
+      ResetPasswordEntity(message: message ?? '', token: token ?? '');
+}
