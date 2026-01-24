635a9f882f8b55f6557f2be6d8a887451b971049
Ahmed Yahia <ahmed22y10@gmail.com>
2026-01-24 21:28:39 +0200

    feat(cart-data-sources): Implement cart feature with remote data sources, and tests.

---
+++ lib/features/cart/api/data_sources/cart_remote_data_source_impl.dart
@@ -0,0 +1,38 @@
+import 'package:flower_app/features/cart/api/api_clients/cart_api_client.dart';
+import 'package:flower_app/features/cart/data/datasources/cart_remote_data_source.dart';
+import 'package:flower_app/features/cart/data/models/add_to_cart_request_model.dart';
+import 'package:flower_app/features/cart/data/models/clear_cart_response_model.dart';
+import 'package:flower_app/features/cart/data/models/get_cart_response_model/get_cart_response_model.dart';
+import 'package:flower_app/features/cart/data/models/update_item_quantity_request_model.dart';
+import 'package:injectable/injectable.dart';
+
+@Injectable(as: CartRemoteDataSource)
+class CartRemoteDataSourceImpl implements CartRemoteDataSource {
+  final CartApiClient _apiClient;
+  CartRemoteDataSourceImpl(this._apiClient);
+
+  @override
+  Future<GetCartResponseModel> getCart() {
+    return _apiClient.getCart();
+  }
+
+  @override
+  Future<GetCartResponseModel> addToCart({required AddToCartRequestModel requestModel}) {
+    return _apiClient.addToCart(body: requestModel);
+  }
+
+  @override
+  Future<ClearCartResponseModel> clearCart() {
+    return _apiClient.clearCart();
+  }
+
+  @override
+  Future<GetCartResponseModel> removeItemFromCart({required String productId}) {
+    return _apiClient.removeItemFromCart(productId: productId);
+  }
+
+  @override
+  Future<GetCartResponseModel> updateItemQuantity({required String productId, required UpdateItemQuantityRequestModel requestModel}) {
+    return _apiClient.updateItemQuantity(productId: productId, body: requestModel);
+  }
+}
---
+++ test/features/cart/api/data_sources/cart_remote_data_source_impl_test.dart
@@ -0,0 +1,296 @@
+import 'package:flower_app/features/cart/api/data_sources/cart_remote_data_source_impl.dart';
+import 'package:flower_app/features/cart/data/datasources/cart_remote_data_source.dart';
+import 'package:flower_app/features/cart/data/models/add_to_cart_request_model.dart';
+import 'package:flower_app/features/cart/data/models/cart_model/cart_item_model.dart';
+import 'package:flower_app/features/cart/data/models/cart_model/cart_model.dart';
+import 'package:flower_app/features/cart/data/models/clear_cart_response_model.dart';
+import 'package:flower_app/features/cart/data/models/get_cart_response_model/get_cart_response_model.dart';
+import 'package:flower_app/features/cart/data/models/update_item_quantity_request_model.dart';
+import 'package:test/test.dart';
+import 'package:flower_app/features/cart/api/api_clients/cart_api_client.dart';
+import 'package:mockito/annotations.dart';
+import 'package:mockito/mockito.dart';
+
+import 'cart_remote_data_source_impl_test.mocks.dart';
+
+@GenerateMocks([CartApiClient])
+void main() {
+  late CartRemoteDataSource cartRemoteDataSource;
+  late MockCartApiClient mockCartApiClient;
+
+  const tProductId = '1';
+  const tQuantity = 2;
+  final tAddToCartRequestModel = AddToCartRequestModel(
+    productId: tProductId,
+    quantity: tQuantity,
+  );
+  final tUpdateItemQuantityRequestModel = UpdateItemQuantityRequestModel(
+    quantity: tQuantity,
+  );
+  final tGetCartResponseModel = GetCartResponseModel(
+    message: 'Cart fetched successfully',
+    numOfCartItems: 1,
+    cart: CartModel(
+      id: '1',
+      user: '1',
+      cartItems: [
+        CartItemModel(
+          id: '1',
+          product: CartProductModel(
+            id: '1',
+            title: 'Product 1',
+            slug: 'product-1',
+            description: 'Description 1',
+            price: 100,
+            images: ['image-1'],
+            imgCover: 'image-1',
+            category: '1',
+            isSuperAdmin: true,
+            occasion: 'occasion',
+            priceAfterDiscount: 100,
+            quantity: 10,
+            productId: tProductId,
+            rateAvg: 2,
+            rateCount: 2,
+            v: tQuantity,
+            sold: 100,
+            createdAt: DateTime.now(),
+            updatedAt: DateTime.now(),
+          ),
+          quantity: 1,
+          price: 100,
+        ),
+      ],
+      createdAt: DateTime.now(),
+      updatedAt: DateTime.now(),
+      appliedCoupons: const [],
+      totalPrice: 20,
+      v: 2,
+    ),
+  );
+  final tClearCartResponseModel = ClearCartResponseModel(
+    message: 'Cart cleared successfully',
+  );
+
+  void verifyOnlyThisCall(Function verification, dynamic mockObject) {
+    verification(); // Execute the verification
+    verifyNoMoreInteractions(mockObject);
+  }
+
+  setUpAll(() {
+    mockCartApiClient = MockCartApiClient();
+    cartRemoteDataSource = CartRemoteDataSourceImpl(mockCartApiClient);
+  });
+
+  setUp(() {
+    reset(mockCartApiClient);
+  });
+
+  group('getCart', () {
+    test(
+      'should return GetCartResponseModel when the API call is successful',
+      () async {
+        when(
+          mockCartApiClient.getCart(),
+        ).thenAnswer((_) async => tGetCartResponseModel);
+
+        final result = await cartRemoteDataSource.getCart();
+
+        expect(result, isA<GetCartResponseModel>());
+        expect(result, tGetCartResponseModel);
+
+        verifyOnlyThisCall(
+          () => verify(mockCartApiClient.getCart()).called(1),
+          mockCartApiClient,
+        );
+      },
+    );
+    test('should throw an exception when the API call fails', () async {
+      when(mockCartApiClient.getCart()).thenThrow(Exception('API call failed'));
+
+      expect(() async => await cartRemoteDataSource.getCart(), throwsException);
+      verifyOnlyThisCall(
+        () => verify(mockCartApiClient.getCart()).called(1),
+        mockCartApiClient,
+      );
+    });
+  });
+
+  group('addToCart', () {
+    test(
+      'should return GetCartResponseModel when the API call is successful',
+      () async {
+        when(
+          mockCartApiClient.addToCart(body: tAddToCartRequestModel),
+        ).thenAnswer((_) async => tGetCartResponseModel);
+
+        final result = await cartRemoteDataSource.addToCart(
+          requestModel: tAddToCartRequestModel,
+        );
+
+        expect(result, isA<GetCartResponseModel>());
+        expect(result, tGetCartResponseModel);
+
+        verifyOnlyThisCall(
+          () => verify(
+            mockCartApiClient.addToCart(body: tAddToCartRequestModel),
+          ).called(1),
+          mockCartApiClient,
+        );
+      },
+    );
+    test('should throw an exception when the API call fails', () async {
+      when(
+        mockCartApiClient.addToCart(body: tAddToCartRequestModel),
+      ).thenThrow(Exception('API call failed'));
+
+      expect(
+        () async => await cartRemoteDataSource.addToCart(
+          requestModel: tAddToCartRequestModel,
+        ),
+        throwsException,
+      );
+      verifyOnlyThisCall(
+        () => verify(
+          mockCartApiClient.addToCart(body: tAddToCartRequestModel),
+        ).called(1),
+        mockCartApiClient,
+      );
+    });
+  });
+
+  group('updateItemQuantity', () {
+    test(
+      'should return GetCartResponseModel when the API call is successful',
+      () async {
+        when(
+          mockCartApiClient.updateItemQuantity(
+            productId: tProductId,
+            body: tUpdateItemQuantityRequestModel,
+          ),
+        ).thenAnswer((_) async => tGetCartResponseModel);
+
+        final result = await cartRemoteDataSource.updateItemQuantity(
+          productId: tProductId,
+          requestModel: tUpdateItemQuantityRequestModel,
+        );
+
+        expect(result, isA<GetCartResponseModel>());
+        expect(result, tGetCartResponseModel);
+
+        verifyOnlyThisCall(
+          () => verify(
+            mockCartApiClient.updateItemQuantity(
+              productId: tProductId,
+              body: tUpdateItemQuantityRequestModel,
+            ),
+          ).called(1),
+          mockCartApiClient,
+        );
+      },
+    );
+    test('should throw an exception when the API call fails', () async {
+      when(
+        mockCartApiClient.updateItemQuantity(
+          productId: tProductId,
+          body: tUpdateItemQuantityRequestModel,
+        ),
+      ).thenThrow(Exception('API call failed'));
+
+      expect(
+        () async => await cartRemoteDataSource.updateItemQuantity(
+          productId: tProductId,
+          requestModel: tUpdateItemQuantityRequestModel,
+        ),
+        throwsException,
+      );
+      verifyOnlyThisCall(
+        () => verify(
+          mockCartApiClient.updateItemQuantity(
+            productId: tProductId,
+            body: tUpdateItemQuantityRequestModel,
+          ),
+        ).called(1),
+        mockCartApiClient,
+      );
+    });
+  });
+
+  group('removeItem', () {
+    test(
+      'should return GetCartResponseModel when the API call is successful',
+      () async {
+        when(
+          mockCartApiClient.removeItemFromCart(productId: tProductId),
+        ).thenAnswer((_) async => tGetCartResponseModel);
+
+        final result = await cartRemoteDataSource.removeItemFromCart(
+          productId: tProductId,
+        );
+
+        expect(result, isA<GetCartResponseModel>());
+        expect(result, tGetCartResponseModel);
+
+        verifyOnlyThisCall(
+          () => verify(
+            mockCartApiClient.removeItemFromCart(productId: tProductId),
+          ).called(1),
+          mockCartApiClient,
+        );
+      },
+    );
+    test('should throw an exception when the API call fails', () async {
+      when(
+        mockCartApiClient.removeItemFromCart(productId: tProductId),
+      ).thenThrow(Exception('API call failed'));
+
+      expect(
+        () async => await cartRemoteDataSource.removeItemFromCart(
+          productId: tProductId,
+        ),
+        throwsException,
+      );
+      verifyOnlyThisCall(
+        () => verify(
+          mockCartApiClient.removeItemFromCart(productId: tProductId),
+        ).called(1),
+        mockCartApiClient,
+      );
+    });
+  });
+
+  group('clearCart', () {
+    test(
+      'should return ClearCartResponseModel when the API call is successful',
+      () async {
+        when(
+          mockCartApiClient.clearCart(),
+        ).thenAnswer((_) async => tClearCartResponseModel);
+
+        final result = await cartRemoteDataSource.clearCart();
+
+        expect(result, isA<ClearCartResponseModel>());
+        expect(result, tClearCartResponseModel);
+
+        verifyOnlyThisCall(
+          () => verify(mockCartApiClient.clearCart()).called(1),
+          mockCartApiClient,
+        );
+      },
+    );
+    test('should throw an exception when the API call fails', () async {
+      when(
+        mockCartApiClient.clearCart(),
+      ).thenThrow(Exception('API call failed'));
+
+      expect(
+        () async => await cartRemoteDataSource.clearCart(),
+        throwsException,
+      );
+      verifyOnlyThisCall(
+        () => verify(mockCartApiClient.clearCart()).called(1),
+        mockCartApiClient,
+      );
+    });
+  });
+}
