9d0756d07a4db2c6d2c259adb7da96248a21aebd
Osama Saad <117564564+osama25s@users.noreply.github.com>
2026-01-28 16:16:45 +0200

    fix: login repo impl  Success test case

--- lib/features/categories/api/api_service/categories_api_client.dart
+++ lib/features/categories/api/api_service/categories_api_client.dart
@@ -4,8 +4,6 @@ import 'package:flower_app/features/categories/data/models/get_products_models/c
 import 'package:injectable/injectable.dart';
 import 'package:flower_app/core/constants/api_constants.dart';
 import 'package:flower_app/features/categories/data/models/get_all_categories_models/get_all_categories_response_model.dart';
-import 'package:flower_app/features/categories/data/models/get_products_models/category_product_response.dart';
-import 'package:injectable/injectable.dart';
 import 'package:retrofit/retrofit.dart';
 
 part 'categories_api_client.g.dart';
