571731ea690c45629786a28bed4620e58cdda1d3
‪Ahmed Salah Ahmed‬‏ <AhmedSalah.92.2025@gmail.com>
2026-02-03 09:06:47 +0200

    fix(app-language): Add localization generation script and update localization configuration
    - **New Script:**
        - Added `generate_localization.bat` to automate generating localization keys using `easy_localization`.
    
    - **Localization Configuration:**
        - Updated `EasyLocalization` setup in `main.dart` to detect device locale automatically by setting `startLocale` to `null`.
        - Set `useOnlyLangCode` to `true` and disabled `saveLocale` to ensure the app follows device settings.
        - Implemented a `localeResolutionCallback` in `FlowerApp` to handle supported locale matching and provide a fallback to English.
    
    - **Clean up:**
        - Refined `translationsPath` in `app_asset.dart` by removing a trailing slash.
    
    Signed-off-by: ‪Ahmed Salah Ahmed‬‏ <AhmedSalah.92.2025@gmail.com>

--- lib/flower_app.dart
+++ lib/flower_app.dart
@@ -3,6 +3,8 @@ import 'package:flower_app/core/routing/app_router.dart';
 import 'package:flower_app/core/theme/app_theme.dart';
 import 'package:flutter/material.dart';
 
+import 'core/constants/app_text_constants.dart';
+
 class FlowerApp extends StatelessWidget {
   const FlowerApp({super.key});
 
@@ -16,6 +18,18 @@ class FlowerApp extends StatelessWidget {
       localizationsDelegates: context.localizationDelegates,
       supportedLocales: context.supportedLocales,
       locale: context.locale,
+      localeResolutionCallback: (locale, supportedLocales) {
+        // Check if the current device locale is supported
+        if (locale != null) {
+          for (var supportedLocale in supportedLocales) {
+            if (supportedLocale.languageCode == locale.languageCode) {
+              return supportedLocale;
+            }
+          }
+        }
+        // Fallback to English if device locale is not supported
+        return const Locale(AppTextConstants.enLangKey);
+      },
     );
   }
 }
